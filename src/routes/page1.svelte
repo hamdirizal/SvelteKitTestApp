c0560f8eaadebb0d940d5c65f68cc079b400f925
Hamdi Rizal <hamdirizal@gmail.com>
2022-01-07 14:27:11 +0700

    First commit

---
+++ src/routes/page1.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+export async function load({ url, params, status, fetch }) {
+	return {
+		props: {
+			url: url,
+			params: params,
+		},
+	}
+}
+</script>
+<script>
+	export let url
+	export let params
+	console.log('url',url)
+	console.log('params',params)
+</script>
+<h1>Page 1</h1>
