b43bcac5bc29028665411a9939dc1052a20b4f4c
Hamdi Rizal <hamdirizal@gmail.com>
2022-01-10 23:58:10 +0700

    Renamed page2

--- src/routes/page1.svelte
+++ src/routes/page1.svelte
@@ -1,16 +1,1 @@
-<script context="module">
-export async function load({ url, params, status, fetch }) {	
-	const res = await fetch("https://api.shelfgenie.io/website-settings")
-	if(res && res.ok){
-		const resData = await res.json()
-		return {props: {websiteSettings: resData } };
-	}
-
-	return {props: {websiteSettings: null } };
-}
-</script>
-<script>
-	export let websiteSettings
-	console.log('websiteSettings',websiteSettings)
-</script>
-<h1>Page 1</h1>
+<h1>Page One</h1>
