31a56cb94aa0788ec4db1e7eb5d1d22f485148ab
Hamdi Rizal <hamdirizal@gmail.com>
2022-01-10 23:57:16 +0700

    Added page2

---
+++ src/routes/page-2.svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+	/**
+  * @type {import('@sveltejs/kit').Load}
+  */
+	export async function load({ page, error, status }) {
+
+		const res: Response = await fetch("https://api.shelfgenie.io/website-settings")
+
+    if (res.ok) { 
+			return { props: { websiteSettings: await res.json() } }
+		}
+		else{
+			return { props: { websiteSettings: null } }
+		}
+	}
+</script>
+
+<script>
+	export let websiteSettings
+	console.log('websiteSettings',websiteSettings)
+</script>
+
+<h1>Welcome to Page Two</h1>
+<p>This is a test page version 1</p>
